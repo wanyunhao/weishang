9fbc88d268f33753e22b7887d4006b1b27f9327a
allen_wyh <wyhaizw@gmail.com>
2020-07-23 17:27:31 +0800

    开始搞支付宝

--- src/classes/index/zfb/Common/ZFBBaseVC.js
+++ src/classes/index/zfb/Common/ZFBBaseVC.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {StyleSheet,} from 'react-native';
 import BaseVC from "./BaseVC";
 
-export default class WXBaseVC extends BaseVC {
+export default class ZFBBaseVC extends BaseVC {
 
     componentDidMount() {
         this._setBarStyle(2);
